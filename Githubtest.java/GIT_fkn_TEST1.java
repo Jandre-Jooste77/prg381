97df6b25cf67dbaa4a34fee4637b54bd6043b0b8
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 11:16:05 +0200

    change to text

--- Githubtest.java/GIT_fkn_TEST1.java
+++ Githubtest.java/GIT_fkn_TEST1.java
@@ -1,7 +1,7 @@
 public class GIT_fkn_TEST1 {
     
     public static void main(String[] args) {
-            System.out.println("1st Tollie Java file on git");
+            System.out.println(" TextText");
 
             System.out.println("1st Tollie Java file on git");
             //asdas
