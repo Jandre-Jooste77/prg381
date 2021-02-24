5cd8ad8d5d980465d4be218393786ed7be9421cd
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 10:59:52 +0200

    change 1st to first

--- Githubtest.java/Test1.java
+++ Githubtest.java/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
     
     public static void main(String[] args) {
-            System.out.println("1st Java file on git");
+            System.out.println("first Java file on git");
             //asdas
     }
     
