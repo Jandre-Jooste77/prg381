292b30084c67daf6846a9284801d2f189aeff745
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 11:31:13 +0200

    Poes

--- Githubtest.java/GIT_fkn_TEST1.java
+++ Githubtest.java/GIT_fkn_TEST1.java
@@ -1,7 +1,7 @@
 public class GIT_fkn_TEST1 {
     
     public static void main(String[] args) {
-            System.out.println("iNFO");
+            System.out.println("Groot Hare");
 
             System.out.println("1st Java file on git");
             //asdas
