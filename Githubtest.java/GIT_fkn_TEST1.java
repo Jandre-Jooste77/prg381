27d7f24f0463a9a649f80b0caa618fc107c50966
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 11:33:54 +0200

    Verander

--- Githubtest.java/GIT_fkn_TEST1.java
+++ Githubtest.java/GIT_fkn_TEST1.java
@@ -4,7 +4,7 @@ public class GIT_fkn_TEST1 {
             System.out.println("Fokkit");
 
             System.out.println("1st Java file on git");
-            //asdas
+          
     }
     
     }
