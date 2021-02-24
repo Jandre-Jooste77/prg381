2bb126940cc05a59c85edabe6b31d35f16c4c9d5
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 11:13:13 +0200

    Copy

---
+++ Githubtest.java/GIT_fkn_TEST1.java
@@ -0,0 +1,11 @@
+public class GIT_fkn_TEST1 {
+    
+    public static void main(String[] args) {
+            System.out.println("1st Tollie Java file on git");
+
+            System.out.println("1st Tollie Java file on git");
+            //asdas
+    }
+    
+    }
+    
