55a0564161a1ff334767568d948cee35766b86d0
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-02-24 11:23:33 +0200

    Changed to Info

--- Githubtest.java/GIT_fkn_TEST1.java
+++ Githubtest.java/GIT_fkn_TEST1.java
@@ -1,9 +1,9 @@
 public class GIT_fkn_TEST1 {
     
     public static void main(String[] args) {
-            System.out.println("Data");
+            System.out.println("iNFO");
 
-            System.out.println("1st Tollie Java file on git");
+            System.out.println("1st Java file on git");
             //asdas
     }
     
